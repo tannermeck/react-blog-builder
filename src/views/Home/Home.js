d2a5247d252841773e43d14ba71073fd8d9bc871
TANNER MECK <steve.meck246@gmail.com>
2021-11-29 12:51:16 -0800

    subtitle and font now working

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,12 +8,21 @@ import './Home.css'
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState("Architect's Daughter")
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} setTitle={setTitle} />
+      <Preview title={title} subtitle={subtitle} font={font} />
+      <Editor
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        font={font}
+        setFont={setFont}
+      />
     </main>
   )
 }
